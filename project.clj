cbebaff3fe7f4ccccee5bf34f89c2dd4fb0e55d0
Mike Konkov <noospheratum@gmail.com>
2018-06-01 18:14:08 +0200

    Vbump

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-antlr "0.2.5-SNAPSHOT"
+(defproject d0x-mike/clj-antlr "0.2.7-SNAPSHOT"
   :description "Clojure bindings for the ANTLR 4 parser library."
-  :url "http://github.com/aphyr/clj-antlr"
+  :url "http://github.com/wambat/clj-antlr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.antlr/antlr4-runtime "4.5.3"]
-                 [org.antlr/antlr4 "4.5.3"]]
+                 [org.antlr/antlr4-runtime "4.7.1"]
+                 [org.antlr/antlr4 "4.7.1"]]
   :profiles {:dev {:dependencies
                    [[criterium "0.4.4"]
                     [cheshire "5.6.3"]
